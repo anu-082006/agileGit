fce9773065723f021481e390bf6b70178d3cf21e
anu-082006 <annapurnas2006@gmail.com>
2026-02-19 11:40:07 +0530

    Initialize RandomForestRegressor with sample data
    Added sample training data and improved model initialization.

--- Randomforest.py
+++ Randomforest.py
@@ -2,7 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-RandomForestRegModel = RandomForestRegressor()
-RandomForestRegModel.fit(X,y)
-X_marks=[[70]]
+
+# Sample training data
+X = np.array([[50], [60], [70], [80], [90]])
+y = np.array([55, 65, 75, 85, 95])
+
+# Model
+RandomForestRegModel = RandomForestRegressor(random_state=42)
+RandomForestRegModel.fit(X, y)
+
+# Prediction
+X_marks = [[70]]
 print(RandomForestRegModel.predict(X_marks))
